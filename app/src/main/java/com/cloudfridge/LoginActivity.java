460e74dff2275ecb217b4be5fdcbb0fb38077de7
caogreg <cao.gregory@gmail.com>
2020-10-25 16:16:13 -0400

    Added intent from login towards empty MainActivity

--- app/src/main/java/com/cloudfridge/LoginActivity.java
+++ app/src/main/java/com/cloudfridge/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.cloudfridge;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -24,6 +25,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import java.util.HashMap;
@@ -81,14 +83,14 @@ public class LoginActivity extends AppCompatActivity {
                                 if (document.getId().equals(username)) {
                                     if (password.equals(document.get("password").toString())) {
                                         Log.d(TAG,"Logging in user " + username);
-                                        //TODO: Add Intent to new Activity here
-                                        //TODO: Shared preferences?
+                                        //TODO: Store user info in Shared preferences
+                                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                        startActivity(intent);
                                     }
                                     else {
                                         Toast.makeText(LoginActivity.this, "Incorrect Password.\nPlease try again.", Toast.LENGTH_SHORT).show();
                                     }
-                                }
-                                else{
+                                } else {
                                     Log.d(TAG,document.getId() + " => " + document.getData());
                                 }
                             }
@@ -129,28 +131,8 @@ public class LoginActivity extends AppCompatActivity {
                 .addOnSuccessListener(documentReference -> Log.d(TAG, "DocumentSnapshot added"))
                 .addOnFailureListener(e -> Log.w(TAG, "Error writing document", e));
 
-        //TODO: Add Intent to new Activity here
-        //TODO: Shared preferences?
+        //TODO: Store user info in Shared preferences
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
     }
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
 }
